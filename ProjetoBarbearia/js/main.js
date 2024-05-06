a773ebe9c59de6e9a4c8f332b42d7dafd01440fe
DouglasGsilva <goDouglasdev@gmail.com>
2024-05-06 19:52:40 -0300

    adição do cadastro fixo

--- ProjetoBarbearia/js/main.js
+++ ProjetoBarbearia/js/main.js
@@ -1,6 +1,14 @@
-document
-  .getElementById("agendarCorteBtn")
-  .addEventListener("click", function () {
-    var cadModal = new bootstrap.Modal(document.getElementById("cadModal"));
-    cadModal.show();
-  });
+const openCad = document.querySelector("#agendarCorteBtn");
+const modal = document.querySelector(".modal-Content");
+const btnFechar = document.querySelector(".btn-fechar");
+const overlay = document.querySelector(".overlay");
+
+openCad.addEventListener("click", () => {
+  modal.style.display = "block";
+  modal.style.position = "fixed";
+  overlay.style.display = "block";
+});
+
+btnFechar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
