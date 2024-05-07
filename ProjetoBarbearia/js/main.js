29849ca0ac9383d37035bf3d83b35ca90152cdf2
DouglasGsilva <goDouglasdev@gmail.com>
2024-05-07 17:52:08 -0300

    adicao de outro carousel

--- ProjetoBarbearia/js/main.js
+++ ProjetoBarbearia/js/main.js
@@ -1,14 +1,20 @@
 const openCad = document.querySelector("#agendarCorteBtn");
 const modal = document.querySelector(".modal-Content");
 const btnFechar = document.querySelector(".btn-fechar");
-const overlay = document.querySelector(".overlay");
+const linkCad = document.querySelector("#link-cad");
+const fade = document.querySelector(".modal-fade");
 
 openCad.addEventListener("click", () => {
   modal.style.display = "block";
   modal.style.position = "fixed";
-  overlay.style.display = "block";
+  fade.style.display = "block";
+});
+
+linkCad.addEventListener("click", () => {
+  modal.style.display = "block";
 });
 
 btnFechar.addEventListener("click", () => {
   modal.style.display = "none";
+  fade.style.display = "none";
 });
